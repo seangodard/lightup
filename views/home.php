9631843cdf4ef153d268a9639bff434d7e9c6b60
Sean Godard <godard.sean@gmail.com>
2016-04-06 11:45:06 -0400

    Initial Commit
    Created the MVC layout and began work on the login/registration homepage
    php file.

---
+++ views/home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>LightUp: Login/Register</title>
+	</head>
+	<body>
+		<h1>LightUp</h1>
+		<div class="sidebar">
+			<h2>Login</h2>
+			<input type="text" value="Username" id="login_name">
+			<input type="text" value="Password" id="login_pass">
+			<input type="button" value="Login" id="login">
+			<h2>Register</h2>
+			<input type="text" value="Username" id="register_name">
+			<input type="text" value="Password" id="register_pass">
+			<input type="text" value="Confirm Password" id="register_cpass">
+			<input type="button" value="Register" id="register">
+		</div>
+		<div class="main_body">
+			<h1></h1>
+			<img src="https://placehold.it/350x150" alt="tree"/>
+			<p>
+				Welcome to LightUp! Have you ever had an idea but never believed that anyone would be interested 
+				in it or wondered if maybe someone was already working on it and you just want to join in? Before
+				LightUp, we were suffering from the same problem......  
+			</p>
+		</div>
+	</body>
+</html>
