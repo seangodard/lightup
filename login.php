52ed062d4c4c5478c83b059d612a238f8972151a
Sean Godard <godard.sean@gmail.com>
2016-04-10 18:01:55 -0400

    Added basic login and refactored home page.
    Enabled basic login validation on the homepage. Also refactored the
    login page so that pressing enter on any of the inputs will run the
    AJAX.

--- login.php
+++ login.php
@@ -3,14 +3,14 @@
 require_once('constants.php');
 require_once('models/lib.php');
 
-// TODO : Set up session once logged in/ registered : Fri 08 Apr 2016 08:19:57 PM EDT 
-// TODO : Note that a registered user is automatically logged in : Fri 08 Apr 2016 08:19:57 PM EDT 
+// TODO : Set up session once logged in/registered : Fri 08 Apr 2016 08:19:57 PM EDT 
+// TODO : Make an optional button to keep the user logged in with cookies : Fri 08 Apr 2016 08:19:57 PM EDT 
 // ------------------------------------------------------------------
-// Attempt to register the user with the given username and password
+// Attempt to login the user with the given username and password
 // 	returns a JSON object of the form {stat : boolean, message : message}
 // ------------------------------------------------------------------
 if (isset($_POST['username']) && isset($_POST['pass'])) {
-	echo json_encode(registerUser($_POST['username'], $_POST['pass']));
+	echo json_encode(validateLogin($_POST['username'], $_POST['pass']));
 }
 else {
 	echo json_encode(array('stat' => 'false', 'message' => 'Blank entries are not allowed.'));
--- models/lib.php
+++ models/lib.php
@@ -6,21 +6,24 @@ require_once('models/db_connection.php');
  */
 
 // ------------------------------------------------------------------
-// Attempt to add the given user to the database with the hashed password and a salt
+// Attempt to add the given user to the database with the hashed password and a salt.
 // @param username the username to attempt to register for the user
 // @param password the password to hash and store in the database
-// @return an array of the form {stat => boolean, message => message} 
+// @return an array of the form {stat => boolean, message => message} where stat is whether
+// 	the function succeeded
 // -------------------------------------------------------------------
 function registerUser($username, $password) {
 	$salt_length = 10;
 	// Connect to the database and make sure the connection works before continuing
 	$db = databaseConnection();
-	if ($db === null) { return array('stat' => false, 'message' => 'Could not connect to the database'); }
+	if ($db === null) { return array('stat' => 'false', 'message' => 'Could not connect to the database'); }
 
 	// Make sure that the username is the correct length
 	if (strlen($_POST['username']) < MIN_USERNAME_LENGTH) {
 		return array('stat'=> 'false', 'message' => 'Username taken');
 	}
+
+	// TODO : Add checks to double check that username and pass are not blank : Sun 10 Apr 2016 03:33:19 PM EDT 
 
 	// Check to make sure the username is not already taken 
 	$check_username = $db->prepare('SELECT username FROM users WHERE username = :username');
@@ -61,4 +64,49 @@ function getSalt($length) {
 	}
 
 	return $salt;
+}
+
+// ------------------------------------------------------------------
+// Attempt to validate the user login credentials.
+// @param username the username of the user
+// @param pass the password of the user
+// @return an array of the form {stat => boolean, message => message} where stat is whether
+// 	the function succeeded
+// -------------------------------------------------------------------
+function validateLogin($username, $pass) {
+	// Connect to the database and make sure the connection works before continuing
+	$db = databaseConnection();
+	if ($db === null) { return array('stat' => 'false', 'message' => 'Could not connect to the database'); }
+
+	// TODO : Add checks to make sure that username and pass are not blank : Sun 10 Apr 2016 03:33:41 PM EDT 
+	
+	// Get the salt and hashed_pass for this username
+	$salt_and_hashed = $db->prepare('SELECT salt, hashed_pass FROM users WHERE username = :username');
+	$salt_and_hashed->bindParam(':username', $username);
+	$salt_and_hashed->execute();
+	$salt_and_hashed = $salt_and_hashed->fetchAll();
+
+	// Handle case where the username doesn't exist in the database
+	if (count($salt_and_hashed) == 0) {
+		return array('stat' => 'false', 'message' => 'No user with the given username.');
+	}
+	// Store the salt and hashed_pass that were stored in the DB for this user
+	$salt = $salt_and_hashed[0]['salt'];
+	$stored_hashed_pass = $salt_and_hashed[0]['hashed_pass'];
+
+	// Re-salt and hash the given pass
+	$this_hashed_pass = $db->prepare('SELECT SHA2(:saltedpass, 512) as hashed_pass');
+	$this_pass_salted = $salt.$pass;
+	$this_hashed_pass->bindParam(':saltedpass', $this_pass_salted);
+	$this_hashed_pass->execute();
+	$this_hashed_pass = $this_hashed_pass->fetchAll();
+	$this_hashed_pass = $this_hashed_pass[0]['hashed_pass'];
+
+	// Return wether or not they newly hashed pass matches the one stored
+	if ($this_hashed_pass === $stored_hashed_pass) {
+		return array('stat' => 'true', 'message' => 'Correct password.');
+	}
+	else {
+		return array('stat' => 'false', 'message' => 'Incorrect password.');
+	}
 }
