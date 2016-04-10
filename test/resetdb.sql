e2f9c311127ea5a5df05c9495d0b17a2cc3caa2b
Sean Godard <godard.sean@gmail.com>
2016-04-10 14:40:42 -0400

    Updated DB connection and schema
    Modified the DB connection to use authentication information in the
    file '../lightup_mysql.php' instead of '../mysql.php' to use the lightup
    database. Also, updated the database schema to be able to store the salt
    used to hash passwords. Finally, added some test accounts to use.

--- test/resetdb.sql
+++ test/resetdb.sql
@@ -2,16 +2,25 @@ use lightup;
 
 drop table if exists users;
 
+/* Note: this will automatically create an index on the username since it is unique */
+/* Note: Salting scheme is salt + pass = hashed_pass */
 create table users (
-	username VARCHAR(16) NOT NULL UNIQUE,
-	password VARCHAR(1000) NOT NULL,
 	user_id INT AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(16) NOT NULL UNIQUE,
+	salt VARCHAR(10) NOT NULL,
+	hashed_pass VARCHAR(1000) NOT NULL,
 	CONSTRAINT user_length_check CHECK(LENGTH(username) > 3)
 );
 
-INSERT INTO users(username,password) values("AwesomeSky", ""); /* 1 - coolio */
-INSERT INTO users(username,password) values("sean", "");       /* 2 - pokeball */
-INSERT INTO users(username,password) values("pikachu", "");    /* 3 - ketchup */
-INSERT INTO users(username,password) values("silvEr", "");     /* 4 - money */
-INSERT INTO users(username,password) values("sierra", "");     /* 5 - nevada */
-INSERT INTO users(username,password) values("california", ""); /* 6 - turkey */
+/* username = sky pass = coolio */
+INSERT INTO users(username, salt, hashed_pass) VALUES ("sky", "vSCHdQxwSM", "51ef1d83515589011dac3bd247b40b68e60a5e287d1defb7299e329d49a11aae8381348386ec51962d95635cfd3c373f13295be3bbce166be0ae32a9bdf21819"); 
+/* username = sean pass = watson */
+INSERT INTO users(username, salt, hashed_pass) VALUES ("sean", "wOKOolrUSe", "840aac58f588cf42b9832165cd05a0a93e5f4ce14be05bfb8f99842f40952b694c15d003a38c22a0fdff1cc80823b978f06e688475763236ec6fb9862d593787");
+/* username = alice pass = alpha */
+INSERT INTO users(username, salt, hashed_pass) VALUES ("alice", "fAFYcmhoaJ", "0931a30632b623649f34c4fd79f77e47883c599ef12b66ed7f3967209656c0066ac0011a6d40b36004d7defd185769767ccafe02c4f266557a711f655fd04cc8");
+/* username = bob pass = bravo */
+INSERT INTO users(username, salt, hashed_pass) VALUES ("bob", "aoNUTFuPkw", "3328b52ee2d681498e3666d59ddc83db5b2e1ed69a3b637bc5cec4a453f3cf11ecd65eea759ad7356ed7e4b7707cacf60fbc951d35e402da137c17428e7cc978");
+/* username = craig pass = charlie */
+INSERT INTO users(username, salt, hashed_pass) VALUES ("craig", "esyytsMKmu", "1a85d4c52a7912c30519b632cfaeca3eee215521523953dba2d3d905937f691251b7802b2bb794d708674200416cbd6549ee377ba0ca932611df1b358f808aae");
+/* username = domino pass = delta */
+INSERT INTO users(username, salt, hashed_pass) VALUES ("domino", "gCCIfjBOwB", "3106762d861f84fcaff2f87fb176e7a35840256e8e2eb58d47a69d73daf938c2eeca73f924779360d4b472ab11e6c681e7f54d45befd909d9d086b37c85122aa");
