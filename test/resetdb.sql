d2fef92d1b5069eb62faca3c94f521b9bf210a21
Skyler Ng <SkylerNgC@gmail.com>
2016-04-13 11:56:15 -0400

    Added initial projects & profiles structure.
    Created initial structure for the projects and profiles database along with some test data.

--- test/resetdb.sql
+++ test/resetdb.sql
@@ -5,11 +5,6 @@ drop table if exists users;
 /* Note: this will automatically create an index on the username since it is unique */
 /* Note: Salting scheme is salt + pass = hashed_pass */
 create table users (
-<<<<<<< HEAD
-	username VARCHAR(16) NOT NULL UNIQUE,
-	password VARCHAR(150) NOT NULL,
-=======
->>>>>>> 82fd4f5ff31232624e2ea65b0868a825bd212d82
 	user_id INT AUTO_INCREMENT PRIMARY KEY,
 	username VARCHAR(16) NOT NULL UNIQUE,
 	salt VARCHAR(10) NOT NULL,
@@ -17,25 +12,6 @@ create table users (
 	CONSTRAINT user_length_check CHECK(LENGTH(username) > 3)
 );
 
-<<<<<<< HEAD
-/* 1 - coolio */
-INSERT INTO users(username,password) values("AwesomeSky", "9558f291865a9c4aaa4bc2af5e5e827dfaf39b081dacc72d4ef793737cf7d8fb680fe433faf596ab43cb1f0d12e5c4106aec50ee9281cf666d5fa9b58bd86d71");
-
-/* 2 - pokeball */
-INSERT INTO users(username,password) values("sean", "c59378200f6591d69b9734333bd002142d9a76969d37745fb00efd722050e72495795f8af0ba85bf598f36ed42ec3c1599fef8b15b6b82ea6b2b8a2dbda2c3df");
-
-/* 3 - ketchup */    
-INSERT INTO users(username,password) values("pikachu", "61b6bfe77ae210529ca911d5b87dbd4d31d4a801651bd4a5c5ea5c455b3f4694f114c807e3098daf063ef9686837306c545e9d87a494c9b9b0762f3dfee1c5c6"); 
-
-/* 4 - money */   
-INSERT INTO users(username,password) values("silvEr", "8977fbeabc9c89dba2eca9a66bd2d37df2d21f194b159bca7240544a3f54394ea3c4fb51a129c4b76824e457dc23d6d6b5091745ccb1164645bd2823f91f3764");
-
-/* 5 - nevada */
-INSERT INTO users(username,password) values("sierra", "d39f2532ab585a3198c65f047caa601f026c2c13918f6d5a621578e0527c25b4261c5bf6ea8dcf787c13fa234b3b57ef707016f3cdcf3fd6b5b755944c91af53");
-
-/* 6 - turkey */
-INSERT INTO users(username,password) values("california", "0e971ae6873ecbb3c028aa7295753a5fe13b87c28022c1f5395724eb2bb5f28ca10fc8a41b7f57e3159b96ca2b7f4085456bd572e5f7ac1d48c8a30b14aa1d82");
-=======
 /* username = sky pass = coolio */
 INSERT INTO users(username, salt, hashed_pass) VALUES ("sky", "vSCHdQxwSM", "51ef1d83515589011dac3bd247b40b68e60a5e287d1defb7299e329d49a11aae8381348386ec51962d95635cfd3c373f13295be3bbce166be0ae32a9bdf21819"); 
 /* username = sean pass = watson */
@@ -48,4 +24,38 @@ INSERT INTO users(username, salt, hashed_pass) VALUES ("bob", "aoNUTFuPkw", "332
 INSERT INTO users(username, salt, hashed_pass) VALUES ("craig", "esyytsMKmu", "1a85d4c52a7912c30519b632cfaeca3eee215521523953dba2d3d905937f691251b7802b2bb794d708674200416cbd6549ee377ba0ca932611df1b358f808aae");
 /* username = domino pass = delta */
 INSERT INTO users(username, salt, hashed_pass) VALUES ("domino", "gCCIfjBOwB", "3106762d861f84fcaff2f87fb176e7a35840256e8e2eb58d47a69d73daf938c2eeca73f924779360d4b472ab11e6c681e7f54d45befd909d9d086b37c85122aa");
->>>>>>> 82fd4f5ff31232624e2ea65b0868a825bd212d82
+
+# project(project_id, name, description, picture, creation_timestamp)
+/* TODO: picture is the reference path to the directory containing the image [4/11/16] */
+create table projects (
+	project_id INT AUTO_INCREMENT PRIMARY KEY,
+	project_name VARCHAR(40) NOT NULL UNIQUE,
+	description VARCHAR(1000),
+	picture VARCHAR(500),
+	creation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+INSERT INTO projects(project_name, description, picture) VALUES ("LightUp", "Web Programming Final Project.", "views/lightup.svg");
+INSERT INTO projects(project_name, description, picture) VALUES ("Sky Group, LTD.", "Skyler is awesome club.", "views/sky.jpg");
+INSERT INTO projects(project_name, description, picture) VALUES ("Knitting Needle Katanas", "Web Programming Final Project", "views/lightup.svg");
+
+/* users(user_id, username, password, profile_picture, name, blurb, city, state, country,
+phone, email, experience, skills, hobbies) */
+create table profiles (
+	user_id INT PRIMARY KEY,
+	blurb VARCHAR(1000),
+	city VARCHAR(40),
+	state VARCHAR(2),
+	country VARCHAR(40),
+	phone VARCHAR(10),
+	email VARCHAR(40),
+	experience VARCHAR(40),
+	skills VARCHAR(40),
+	hobbies VARCHAR(40),
+	FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+INSERT INTO profiles(user_id, blurb, city, state, country, phone, email, experience, skills, hobbies) 
+	VALUES (1, "Awesome Person", "New York City", "NY", "USA", "0123456789", "awesome@sky.com", "Over 9000!!!", "Awesomeness", "Being Awesome");
+INSERT INTO profiles(user_id, blurb, city, state, country, phone, email, experience, skills, hobbies) 
+	VALUES (2, "Just another upstanding citizen from Bombay.", "Bombay", "NY", "USA", "3845093782", "arrow@sean.com", "*&!^%*^&%(&^", "Keeping it fresh", "Art");
