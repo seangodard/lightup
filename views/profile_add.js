7817c7a92e24b8226ed90ded05d8daffb45e8c68
Skyler Ng <SkylerNgC@gmail.com>
2016-05-10 12:04:15 -0400

    Modified the add buttons in the edit profile.
    Adding additional input boxes now line up with previous input boxes.

--- views/profile_add.js
+++ views/profile_add.js
@@ -14,21 +14,21 @@ $(document).ready(function() {
 		// Send an AJAX request to add an input box and respond accordinly in HTML
 		$.post('profile_add_drop_box.php', {addSection:section}, function(response) {
 			if (section === 'experiences') {
-				var new_item = '<div id="exp_'+response.id+'" class="info">' +
+				var new_item = '<div id="exp_'+response.id+'" class="info inputRow">' +
 									'<input type="image" id="exp_'+response.id+'" class="drop inputRowButton" src="views/images/minus.svg" alt="drop" width="40" height="40">' +
 									'<input class="rowInput" type="text" name="exp_'+response.id+'" value="'+response.experiences+'"> '+
 								'</div>';
 				$('#experiences').children('#exp_add').before(new_item);
 			}
 			else if (section === 'skills') {
-				var new_item = '<div id="skill_'+response.id+'" class="info">' +
+				var new_item = '<div id="skill_'+response.id+'" class="info inputRow">' +
 									'<input type="image" id="skill_'+response.id+'" class="drop inputRowButton" src="views/images/minus.svg" alt="drop" width="40" height="40">' +
 									'<input class="rowInput" type="text" name="skill_'+response.id+'" value="'+response.skills+'"> '+
 								'</div>';
 				$('#skills').children('#skill_add').before(new_item);
 			}
 			else if (section === 'hobbies') {
-				var new_item = '<div id="hobby_'+response.id+'" class="info">' +
+				var new_item = '<div id="hobby_'+response.id+'" class="info inputRow">' +
 									'<input type="image" id="hobby_'+response.id+'" class="drop inputRowButton" src="views/images/minus.svg" alt="drop" width="40" height="40">' +
 									'<input class="rowInput" type="text" name="hobby_'+response.id+'" value="'+response.hobbies+'"> '+
 								'</div>';
