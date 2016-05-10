153ec6d20831d067b6351bbdf7836f4f41634c09
Sean Godard <godard.sean@gmail.com>
2016-05-09 20:16:33 -0400

    Working on refining page styling.
    Updating the websites styling with a focus on buttons.

--- views/top_bar.php
+++ views/top_bar.php
@@ -9,7 +9,7 @@
 	<div class="header header1">
 		<div id="profile_logout">
 			<a class="profile" href="profile.php?id=<?php echo htmlentities(getLoggedInUserID(), ENT_QUOTES, 'utf-8'); ?>"><input type="image" src="views/images/profile.jpg" alt="profile" width="50" height="50"></a>
-			<a class="logout" href="../logout.php"><input type="image" src="views/images/logout.png" alt="logout" width="50" height="50"></a>
+			<a class="logout" href="../logout.php"><img src="views/images/logout.svg" alt="logout"></a>
 		</div>
 		<div id="search_box">
 			<form action="search.php" method="get">
